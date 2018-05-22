d0d3bd3aeb99c3cf71b923b16722bb278715574b
ffffffuck <chshone88@gmail.com>
2018-05-22 19:54:28 +0900

    dd

--- app/src/main/java/kr/hkit/iotmobile/MenuActivity.java
+++ app/src/main/java/kr/hkit/iotmobile/MenuActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import kr.hkit.iotmobile.preference.AddressPreference;
@@ -17,16 +18,19 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        TextView ledTextview = findViewById(R.id.ledTextView);
+        TextView tvTextview = findViewById(R.id.tvTextView);
+
         Button ledButton = findViewById(R.id.ledButton);
         ledButton.setOnClickListener(onLedListener);
+
+        Button tvButton = findViewById(R.id.tvButton);
+        tvButton.setOnClickListener(onTvListener);
     }
 
     public View.OnClickListener onLedListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
-
-            Toast.makeText(getBaseContext(), "LED!!", Toast.LENGTH_SHORT).show();
 
             AddressPreference ap = new AddressPreference(getBaseContext());
             String ip = ap.getIP();
@@ -37,10 +41,35 @@ public class MenuActivity extends AppCompatActivity {
         }
     };
 
+    public View.OnClickListener onTvListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AddressPreference ap = new AddressPreference(getBaseContext());
+            String ip = ap.getIP();
+            int port = ap.getPort();
+
+            SocketClient socketClient = new SocketClient(ip, port, "TV", socketClientListener);
+            socketClient.execute();
+        }
+    };
+
     private SocketClientListener socketClientListener = new SocketClientListener() {
         @Override
         public void onReceiveMessage(String receiveMessage) {
-            Toast.makeText(getBaseContext(), receiveMessage, Toast.LENGTH_SHORT).show();
+            TextView ledTextview = findViewById(R.id.ledTextView);
+            TextView tvTextview = findViewById(R.id.tvTextView);
+
+            if(receiveMessage.contains("TV ON")){
+                tvTextview.setText("ON");
+            }else if(receiveMessage.contains("TV OFF")){
+                tvTextview.setText(getString(R.string.OFF));
+            }
+
+            if(receiveMessage.contains("LED ON")){
+                ledTextview.setText(getString(R.string.ON));
+            }else if(receiveMessage.contains("LED OFF")){
+                ledTextview.setText(getString(R.string.OFF));
+            }
         }
     };
 }
